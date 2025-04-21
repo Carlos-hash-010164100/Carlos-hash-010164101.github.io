4ae3c7e986c480f8a97d472f804e73aeb0111497
Carlos-hash-010164100 <150502061+Carlos-hash-010164100@users.noreply.github.com>
2025-04-20 22:47:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('boton').addEventListener('click', () => {
     const mensaje = document.getElementById('mensaje');
-    mensaje.textContent = 'Futuro Rapero';
+    mensaje.textContent = 'Buen dia';
 });
