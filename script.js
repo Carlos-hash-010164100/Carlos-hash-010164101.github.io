48641663965f7f20ab76012ac5aff1474fc8fa20
Carlos-hash-010164100 <150502061+Carlos-hash-010164100@users.noreply.github.com>
2025-04-23 22:55:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('boton').addEventListener('click', () => {
     const mensaje = document.getElementById('mensaje');
-    mensaje.textContent = ' Observa ';
+    mensaje.textContent = ' Buen dia ';
 });
