804448ab88ea765f5b5d7c5b9236ad8b0f900243
Carlos-hash-010164100 <150502061+Carlos-hash-010164100@users.noreply.github.com>
2025-04-25 20:54:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('boton').addEventListener('click', () => {
     const mensaje = document.getElementById('mensaje');
-    mensaje.textContent = ' Buen dia ';
+    mensaje.textContent = '❤️'; // Cambiamos el texto por un corazón
 });
