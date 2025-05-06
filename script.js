25a38fa633e93cc603a9e02a96fd089c16434955
Carlos-hash-010164100 <150502061+Carlos-hash-010164100@users.noreply.github.com>
2025-05-05 22:47:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('boton').addEventListener('click', () => {
     const mensaje = document.getElementById('mensaje');
     const musica = document.getElementById('musica'); // Seleccionamos el audio
-    mensaje.textContent = ' ¡ Piensa en lo que ya as construido ! '; // Cambiamos el mensaje
+    mensaje.textContent = ' ¡ Piensa, crea y descubre que ahi todo un mundo de posibilidades. ! '; // Cambiamos el mensaje
     musica.play(); // Reproducimos la música
 });
