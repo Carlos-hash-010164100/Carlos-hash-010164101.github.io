9880321793fca3ea292b831ff197721bc171f086
Carlos-hash-010164100 <150502061+Carlos-hash-010164100@users.noreply.github.com>
2025-05-14 22:56:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.getElementById('boton').addEventListener('click', () => {
     const mensaje = document.getElementById('mensaje');
-    const musica = document.getElementById('musica'); // Seleccionamos el audio
-    mensaje.textContent = 'El sistema te observa... Bienvenido al código.';
-    musica.play(); // Reproducimos el sonido de fondo
+    const musica = document.getElementById('musica'); // Audio motivacional
+    
+    mensaje.textContent = 'No te detengas. Lo mejor está por venir.';
+    musica.play(); // Reproducimos el sonido inspirador
 });
